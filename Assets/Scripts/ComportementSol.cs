08c1f72d8b09aaa718b76aecb66eb7ddcfdc2981
abernard18 <abernard@ece.fr>
2016-01-04 16:41:18 +0100

    2.1 et 2.2 du drive

---
+++ Assets/Scripts/ComportementSol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComportementSol : MonoBehaviour {
+
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Lettre")
+        {
+            //detruit la lettre
+            Destroy(collision.gameObject);
+        }
+
+
+    }
+}
