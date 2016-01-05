900d71eb20b46d415e8f00f103f285bf4d7247d7
PaulECE <rauber@ece.fr>
2016-01-05 19:18:36 +0100

    ajout de la scene de menu et d'option, linkage dès scenes

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    void OnGUI()
+    {
+        const int buttonWidth = 70;
+        const int buttonHeight = 50;
+
+        Rect buttonRect = new Rect(
+            Screen.width / 2 - (buttonWidth / 2),
+            (2 * Screen.height / 3) - (buttonHeight / 2) - 150,
+            buttonWidth,
+            buttonHeight
+            );
+
+        Rect buttonRect1 = new Rect(
+           Screen.width / 2 - (buttonWidth / 2),
+           (2 * Screen.height / 3) - (buttonHeight / 2) - 70,
+           buttonWidth,
+           buttonHeight
+           );
+
+        Rect buttonRect2 = new Rect(
+           Screen.width / 2 - (buttonWidth / 2),
+           (2 * Screen.height / 3) - (buttonHeight / 2)+10,
+           buttonWidth,
+           buttonHeight
+           );
+
+        if(GUI.Button(buttonRect, "JOUER"))
+        {
+            Application.LoadLevel("Scene1.0.1");
+        }
+
+        if (GUI.Button(buttonRect1, "OPTION"))
+        {
+            Application.LoadLevel("Options");
+        }
+
+        if (GUI.Button(buttonRect2, "QUITTER"))
+        {
+            Application.Quit();
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsScript : MonoBehaviour {
+
+    void OnGUI()
+    {
+        const int buttonWidth = 70;
+        const int buttonHeight = 50;
+
+        Rect buttonRect = new Rect(
+             Screen.width / 2 - (buttonWidth / 2),
+             (2 * Screen.height / 3) - (buttonHeight / 2) + 40,
+             buttonWidth,
+             buttonHeight
+             );
+
+        if (GUI.Button(buttonRect, "RETOUR"))
+        {
+            Application.LoadLevel("Menu");
+        }
+
+    }
+}
