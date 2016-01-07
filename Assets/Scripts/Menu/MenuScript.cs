165b3ae59958ef9bbaad46b99e51ed80decde2ef
PaulECE <rauber@ece.fr>
2016-01-07 16:48:33 +0100

    ajout scene partie et refonte du menu

--- Assets/Scripts/Menu/MenuScript.cs
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -3,48 +3,29 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
-    void OnGUI()
+
+    public void butonQuitter()
     {
-        const int buttonWidth = 70;
-        const int buttonHeight = 50;
-
-        Rect buttonRect = new Rect(
-            Screen.width / 2 - (buttonWidth / 2),
-            (2 * Screen.height / 3) - (buttonHeight / 2) - 150,
-            buttonWidth,
-            buttonHeight
-            );
-
-        Rect buttonRect1 = new Rect(
-           Screen.width / 2 - (buttonWidth / 2),
-           (2 * Screen.height / 3) - (buttonHeight / 2) - 70,
-           buttonWidth,
-           buttonHeight
-           );
-
-        Rect buttonRect2 = new Rect(
-           Screen.width / 2 - (buttonWidth / 2),
-           (2 * Screen.height / 3) - (buttonHeight / 2)+10,
-           buttonWidth,
-           buttonHeight
-           );
-
-        if(GUI.Button(buttonRect, "JOUER"))
-        {
-            Application.LoadLevel("Scene1.0.1");
-        }
-
-        if (GUI.Button(buttonRect1, "OPTIONS"))
-        {
-            Application.LoadLevel("Options");
-        }
-
-        if (GUI.Button(buttonRect2, "QUITTER"))
-        {
-            Application.Quit();
-        }
-
+        Application.Quit();
     }
 
+    public void butonJouerNiveaux()
+    {
+        Application.LoadLevel("Scene1.0.1");
+    }
+
+    public void butonJouerPerso()
+    {
+        Application.LoadLevel("Scene1.0.1");
+    }
+
+
+    public void butonOptions()
+    {
+        Application.LoadLevel("Options");
+    }
+
+
+    
 
 }
