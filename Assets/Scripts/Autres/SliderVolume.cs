2bed43d02151bbdf8a932c6a6aa161d0d383d28b
PaulECE <rauber@ece.fr>
2016-01-12 01:16:52 +0100

    ajout son pour scene1.01, slider volume dans options fonctionel, ajout de listes mots alea partie perso

---
+++ Assets/Scripts/Autres/SliderVolume.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderVolume : MonoBehaviour {
+
+   Text text; //pour le slider du volume
+
+  
+
+    void Awake()
+    {
+        // Set up the reference.
+   text = GetComponent<Text>();
+    
+        
+      
+
+    }
+
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+     text.text = "Volume: " + MenuScript.currentVolume;
+    }
+}
