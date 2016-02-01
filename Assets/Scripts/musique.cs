1aa497c11d03d84aa75ddd48cceb8e83ebc600f5
PaulECE <rauber@ece.fr>
2016-02-01 16:55:48 +0100

    fix

---
+++ Assets/Scripts/musique.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musique : MonoBehaviour {
+
+
+    public AudioClip musiquedeFond;
+
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        //source.volume = MenuScript.currentVolume;
+        
+
+    }
+	// Use this for initialization
+	void Start () {
+
+      
+
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!source.isPlaying)
+        {
+
+            source.PlayOneShot(musiquedeFond);
+        }
+	
+	}
+}
