ab8ba0955f69308441eecc56896b59708bce248a
PaulECE <rauber@ece.fr>
2016-02-02 15:46:43 +0100

    slider volumes

--- Assets/Scripts/genvolumemusicslider.cs
+++ Assets/Scripts/genvolumemusicslider.cs
@@ -8,6 +8,11 @@ public class genvolumemusicslider : MonoBehaviour {
     public GameObject sliderMusic;
     public GameObject sliderMusicPrefab;
 
+    public GameObject sliderSon;
+    public GameObject sliderSonPrefab;
+
+
+
    
 
     public Canvas Canvanas;
@@ -15,12 +20,20 @@ public class genvolumemusicslider : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+
+
         sliderMusic = (GameObject) Instantiate(sliderMusicPrefab, new Vector2(0.5f,0.5f), Quaternion.identity);
         sliderMusic.transform.SetParent(Canvanas.transform);
         sliderMusic.GetComponent<Slider>().value = MenuScript.currentMusicVolume;
         sliderMusic.GetComponent<Transform>().localPosition = new Vector2(172,65);
         sliderMusic.GetComponent<Transform>().localScale = new Vector2(3, 3);
-	// 172x 65y 0z 181width 17 height x0.5 y0.5 sclae x3 y3 z1
+
+        sliderSon = (GameObject)Instantiate(sliderSonPrefab, new Vector2(0.5f, 0.5f), Quaternion.identity);
+        sliderSon.transform.SetParent(Canvanas.transform);
+        sliderSon.GetComponent<Slider>().value = MenuScript.currentVolume;
+        sliderSon.GetComponent<Transform>().localPosition = new Vector2(120, 175);
+        sliderSon.GetComponent<Transform>().localScale = new Vector2(3, 3);
+
 
 	}
 	
