ae494f65361ba44fbdeaadf6a044649c185337fe
PaulECE <rauber@ece.fr>
2016-02-02 15:24:46 +0100

    slider volume

---
+++ Assets/Scripts/Autres/SliderVolumeMusic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderVolumeMusic : MonoBehaviour {
+
+
+    Text musicslider; //pour le slider du volume
+
+
+
+    void Awake()
+    {
+        // Set up the reference.
+        musicslider = GetComponent<Text>();
+
+
+
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        musicslider.text = "Volume de la musique : " + MenuScript.currentMusicVolume;
+    }
+}
